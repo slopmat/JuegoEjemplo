e58813c7f1b00c3858006df64c554f43bb7d75c8
slopmat <120409284+slopmat@users.noreply.github.com>
2023-01-09 17:51:18 +0100

    Update Shoot.cs

--- Shoot.cs
+++ Shoot.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 //Cambio1
 //Cambio2
 
 >>>>>>> parent of 76ce700 (hth)
+=======
+>>>>>>> parent of 8d9f23a (Guardado1)
 public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
