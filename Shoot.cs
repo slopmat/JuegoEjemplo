b196447925e5e95de6ceeec505333821cc5ffaf3
slopmat <120409284+slopmat@users.noreply.github.com>
2023-01-09 17:44:28 +0100

    Revert "Guardado2"
    This reverts commit b48868a14028f25f6519406e356ea07631c89f03.

--- Shoot.cs
+++ Shoot.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< HEAD
 //Cambio1
 //Cambio2
+=======
+>>>>>>> parent of 8d9f23a (Guardado1)
 public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
